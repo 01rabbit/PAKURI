6c3162a5c8100d4a34e760d44e678d65dd420252
Mr.Rabbit <appleseedj073@gmail.com>
2019-12-17 15:45:24 +0900

    Add files via upload

---
+++ modules/assist_module.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+source ../source.conf
+source misc_module.sh
+
+function assist_main()
+{
+    box_1 "Scanning"
+    echo -e ""
+    echo -e ""
+    box_2 "Exploit"
+    echo -e ""
+    echo -e ""
+    box_3 "Config"
+    echo -e ""
+    echo -e ""
+}
+assist_main
