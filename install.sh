4682b11a087d027ad57f85aeac445a59b44e708f
01rabbit <appleseedj073@gmail.com>
2019-12-21 02:29:08 +0900

    update

--- install.sh
+++ install.sh
@@ -1,16 +1,14 @@
 #! /bin/bash
 # Install script for PAKURI
 
-BLACK='\033[0;30m'
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
 LIGHTBLUE='\033[0;36m'
 NC='\033[0m'
 
-echo -e "${GREEN_b}"
+echo -e "${GREEN}"
 echo -e " ██▓███        ▄▄▄            ██ ▄█▀      █    ██       ██▀███        ██▓"
 echo -e "▓██░  ██▒     ▒████▄          ██▄█▒       ██  ▓██▒     ▓██ ▒ ██▒     ▓██▒"
 echo -e "▓██░ ██▓▒     ▒██  ▀█▄       ▓███▄░      ▓██  ▒██░     ▓██ ░▄█ ▒     ▒██▒"
@@ -31,7 +29,7 @@ INSTALL_DIR=/usr/share/pakuri
 PLUGINS=/usr/share/pakuri/plugins
 
 mkdir -p $INSTALL_DIR 2> /dev/null
-cp -Rf * $INSTALL_DIR 2> /dev/null
+cp -Rf . $INSTALL_DIR 2> /dev/null
 
 echo -e "${YELLOW}"
 echo -e "Installing package dependencies."
@@ -57,6 +55,6 @@ cd $PLUGINS/AutoRecon && pip3 install -r requirements.txt 2> /dev/null
 chmod +x $INSTALL_DIR/pakuri.sh
 chmod +x $INSTALL_DIR/modules/import-faraday.sh
 
-echo -e "${RED_b}"
+echo -e "${RED}"
 echo -e "Installing Plugins."
 echo -e "${NC}"
