0f836d4d7722961f89e967028d8aa8f0964ed785
01rabbit <appleseedj073@gmail.com>
2019-12-24 22:07:48 +0900

    update

--- modules/import-faraday.sh
+++ modules/import-faraday.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-source source.conf
 # $1 Working Directory
 # $2 Faraday's Workspace name
 
@@ -7,7 +6,7 @@ if [[ ! -d $1/imported ]];then
 	mkdir -p $1/imported
 fi
 
-for i in `find $1 -name $1/\*.xml -not -path "$1/imported/*"`
+for i in `find $1 -path "$1/*.xml" -not -path "$1/imported/*"`
 do
 	faraday-client --cli --workspace $2 --report $i
 	mv $i -t $1/imported/
