79dab69897fd29237ff01446ebd87fbff6865402
Mr.Rabbit <appleseedj073@gmail.com>
2020-01-24 15:11:14 +0900

    Update export_csv.sh

--- modules/export_csv.sh
+++ modules/export_csv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo -u postgres psql -d msf > datatest << _EOF
+sudo -u postgres psql -q -d msf << _EOF
 COPY (select address,os_name,os_sp,purpose,os_family,port,services.proto,
 services.name,services.state,services.info from hosts,services where hosts.id = host_id )
  TO '/tmp/test.csv' with CSV DELIMITER ',' HEADER FORCE QUOTE *;
